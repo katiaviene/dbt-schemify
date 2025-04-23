72ba3bb0ca9f6ee848a842db778a7ac39f546edd
katiaviene <katiacookiefairy@gmail.com>
2025-04-23 19:24:35 +0300

    schema editor in progress: flatten - check, gather - not

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from schema_editor import SchemaEditor
 
-schema_editor = SchemaEditor('schema.yml')
+editor = SchemaEditor('schema.yml')
 
-print(schema_editor.read_schema())
+# Read schema
+editor.read_schema()
+
+# Flatten the schema
+flattened_data = editor.flatten_schema()
+
+# Gather the data back into the dict
+reconstructed_schema = editor.gather_to_dict(flattened_data)
+
+# Update schema_data with the reconstructed version
+editor.schema_data = reconstructed_schema
+
+# Write back to the file
+editor.write_schema()
+
